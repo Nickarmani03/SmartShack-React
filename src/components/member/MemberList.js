f7f8dfa49232f4d2f6e56800f14dc50ded0aa98f
Nickarmani03 <nickarmani03@gmail.com>
2021-06-15 13:14:42 -0500

    working member form

--- src/components/member/MemberList.js
+++ src/components/member/MemberList.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react"
 import { MemberContext } from "./MemberProvider"
 // import { Avatar1 } from "../../IMG/44554.PNG"
 import "./Member.css"
+import { useHistory } from 'react-router-dom'
 
 export const MemberList = () => {
     // This state changes when `getmembers()` is invoked below
@@ -13,8 +14,10 @@ export const MemberList = () => {
         getMembers()
     }, [])
 
-
+    const history = useHistory()
     return (
+        <>
+        <h2>Family Members</h2>
         <section className="familyMembers">
             {
                 members.map(member => {
@@ -24,7 +27,7 @@ export const MemberList = () => {
                                 Name: {member.name}
                             </div>
                             <div className="familyMember__type">
-                                Type: {member.name}
+                                Age: {member.age}
                             </div>
                             {/* <div className="familyMember__type">
                                 Picture: <img src={ Avatar1 }/>
@@ -35,5 +38,10 @@ export const MemberList = () => {
                 })
             }
         </section>
+        <button className="form" onClick={
+                () => history.push("/members/create")}>
+                Add New Member
+            </button>
+        </>
     )
 }
